787603cdea942fb9744e368b9efd3930ba7e219c
kapatblxa <kapatblxa@gmail.com>
2017-08-12 15:01:45 +0300

    add logic of product quantity change

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
 
 
-function setInput(x) {
-	let condomsQuantity = document.querySelector('.condom-set${x}');
+let timerId;
+let condomsModel = ['condom1', 'condom2','condom3','condom4','condom5','condom6'];
+let condomsCount = [0,0,0,0,0,0];
 
+let condomSet0 = document.querySelector('.condom-set0');
+let condomSet1 = document.querySelector('.condom-set1');
+let condomSet2 = document.querySelector('.condom-set2');
+let condomSet3 = document.querySelector('.condom-set3');
+let condomSet4 = document.querySelector('.condom-set4');
+let condomSet5 = document.querySelector('.condom-set5');
+
+condomSet0.addEventListener('mouseover', setInput(0));
+condomSet1.addEventListener('mouseover', setInput(1));
+condomSet2.addEventListener('mouseover', setInput(2));
+condomSet3.addEventListener('mouseover', setInput(3));
+condomSet4.addEventListener('mouseover', setInput(4));
+condomSet5.addEventListener('mouseover', setInput(5));
+
+function setInput(x) {
+	let condomsQuantityArray = document.querySelectorAll('.condom-set');
+	let condomsQuantity = condomsQuantityArray[x];
 	//Actions on input
 	condomsQuantity.addEventListener('click', condomsCount);
 	function condomsCount() {
 	//Condoms quantity input. Element creation.
 		//click action
-		let quantity = document.createElement(`input`); //Области видимости. Сделай quantity видимой для строки№ 31
+		let quantity = document.createElement(`input`);
 		quantity.className = '.condoms-input';
 		quantity.setAttribute('type', 'number')
 		quantity.setAttribute('placeholder', '0')
@@ -18,15 +36,31 @@ function setInput(x) {
 		condomsQuantity.appendChild(quantity);
 		}
 	}
-		//mouseout action
-	condomsQuantity.addEventListener('mouseout', condomsCountZero);
-	function condomsCountZero() {
-		let searchQuantity = document.querySelector('.condoms-input');
-			if (searchQuantity.value === '0') {
-				searchQuantity.remove();
-			}
+//		condomsCount[x] = quantity.value;}  //Здесь надо записать введенное значение в массив CondomsCount
 	}
-}
+		//mouseout action
+		let searchInput = document.querySelector('.choice-box');
+			searchInput.addEventListener('mouseout', condomsCountZero);
+			function condomsCountZero(event) {
+				let target = event.target;
+				if (target.value === '0') {
+							setTimeout(function() {target.remove()}, 1500);
+							} else {}
+//				console.log(condomsCount);
+			}
+
+
+
+//let searchInput = document.querySelector('.condoms-input');
+//searchInput.addEventListener('mouseout', condomsCountZero);
+//function condomsCountZero() {
+//					if (condomsQuantity.value === 0) {
+//						console.log(searchInput[i]);
+//					searchInput[i].remove();
+//				}
+//}
+
+
 
 
 //ПРоверка на ввод числа а не текста!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
